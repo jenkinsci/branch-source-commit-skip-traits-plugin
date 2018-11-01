cfd5172e0f3badab08926698a1513fdae2b96bce
René Scheibe <rene.scheibe@gmail.com>
2018-11-01 15:26:02 +0100

    Make imports consistent

--- scm-trait-commit-skip-common/src/main/java/org/jenkinsci/plugins/scm_filter/BranchCommitSkipTrait.java
+++ scm-trait-commit-skip-common/src/main/java/org/jenkinsci/plugins/scm_filter/BranchCommitSkipTrait.java
@@ -3,7 +3,9 @@ package org.jenkinsci.plugins.scm_filter;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.plugins.git.GitSCM;
 import hudson.scm.SCMDescriptor;
-import jenkins.scm.api.trait.*;
+import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceTrait;
+import jenkins.scm.api.trait.SCMSourceTraitDescriptor;
 
 /**
  * @author witokondoria
